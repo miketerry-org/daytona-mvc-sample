b1790529827668bfccc8ff8a0cd86788bded0a13
Mike Terry <support@miketerry.org>
2025-11-17 00:36:57 -0600

    now suppoing redis sessions

--- index.js
+++ index.js
@@ -4,5 +4,3 @@ import path from "path";
 import { Application } from "daytona-mvc";
 
 const app = new Application();
-console.log("app.config", app.config);
-console.log("tenants", app.tenants.all[0].config);
